2c86d0f58cbb3ffdab9c61f263276f30843edb89
Woosung Song <lego0901@gmail.com>
2021-04-29 19:11:25 +0900

    Compiler Backend: from IR to Assembly

--- src/backend/Backend.h
+++ src/backend/Backend.h
@@ -24,9 +24,6 @@ namespace backend {
 //backend/TargetMachine.cpp
 //---------------------------------------------------------------
 
-class Memory;
-class Register;
-
 class Symbol{
 protected:
   string name;
@@ -43,8 +40,6 @@ class Register : public virtual Symbol {
 public:
   Register(string name): Symbol(name) {}
   ~Register() {}
-  Register* castToRegister() {return this;}
-  Memory* castToMemory() {return NULL;}
 };
 //Memory addresses(GV, )
 class Memory : public virtual Symbol {
@@ -55,22 +50,16 @@ public:
   ~Memory() {}
   Register* getBase();
   int64_t getOffset();
-  Register* castToRegister() {return NULL;}
-  Memory* castToMemory() {return this;}
 };
 class Func : public Symbol {
 public:
   Func(string name): Symbol(name) {}
   ~Func() {}
-  Register* castToRegister() {return NULL;}
-  Memory* castToMemory() {return NULL;}
 };
 class Block : public Symbol {
 public:
   Block(string name): Symbol(name) {}
   ~Block() {}
-  Register* castToRegister() {return NULL;}
-  Memory* castToMemory() {return NULL;}
 };
 
 class TargetMachine {
