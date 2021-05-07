c319f6ebc5a6572db9f7119403d56eda4e4a16b2
ObjectOrientedLife <smsychjy96@gmail.com>
2021-05-07 21:03:38 +0900

    Pattern matching complete

--- src/mergeBasicBlocks.cpp
+++ src/mergeBasicBlocks.cpp
@@ -5,74 +5,86 @@
 #include "llvm/IR/PatternMatch.h"
 #include <set>
 #include <vector>
-
 using namespace llvm;
 using namespace std;
 using namespace llvm::PatternMatch;
 
-// Merging Equivalent Basic Blocks Pass
+class MergeBasicBlocks : public PassInfoMixin<MergeBasicBlocks> {
+private:
+  pair<BasicBlock *, BasicBlock*> classifyMergeType(BasicBlock *BB);
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
+};
 
-// To reduce unnecessary branching cost caused by unconditional branches,
-// merge all the basic blocks that are equivalent to each other. That is,
-// if two basic blocks are linked through an uncondition branch, merge them
-// together according two the following two conditions.
-// 1) BB_1 flows into BB_2 with an unconditional branch
-// 2) BB_1 dominates BB_2
-// If two BBs don't satisfy the second condition, merge the second BB into the
-// first one and keep the second one intact.
-
-// Returns 
-pair<BasicBlock *, BasicBlock*> classifyMergeType(BasicBlock *BB) {
+// Return a pair of mergeable BBs
+pair<BasicBlock *, BasicBlock*> MergeBasicBlocks::classifyMergeType(BasicBlock *BB) {
 
   Instruction *I = BB->getTerminator();
   // If this is not a branch instruction, return
-  if (isa<BranchInst>(I)) { 
-    return;
+  if (!isa<BranchInst>(I)) { 
+    return {nullptr, nullptr};
   }
 
   // Check if this branch instruction is mergeable
   Value *COND;
-  Value *LEFT;
-  Value *RIGHT;
-
-  BasicBlock *BB_LEFT = dyn_cast<BasicBlock>(LEFT);
-  BasicBlock *BB_RIGHT = dyn_cast<BasicBlock>(RIGHT);
-  assert(BB_LEFT != nullptr);
-  assert(BB_RIGHT != nullptr);
-
+  BasicBlock *BB_LEFT;
+  BasicBlock *BB_RIGHT;
   ConstantInt *C;
-  if (match(I, m_Br(m_ConstantInt(C), m_Value(LEFT), m_Value(RIGHT))) &&
+
+  if (match(I, m_Br(m_ConstantInt(C), m_BasicBlock(BB_LEFT), m_BasicBlock(BB_RIGHT))) &&
       C->isOne()) {
-    outs() << BB_LEFT->getName() << ", " << BB_RIGHT->getName() << "\n";
     // br i1 true, label %BB_LEFT, label %BB_RIGHT
+    outs() << "Case1" << BB_LEFT->getName() << "\n";
     return {BB, BB_LEFT};
-  } else if (match(I, m_Br(m_ConstantInt(C), m_Value(LEFT), m_Value(RIGHT))) &&
+  } else if (match(I, m_Br(m_ConstantInt(C), m_BasicBlock(BB_LEFT), m_BasicBlock(BB_RIGHT))) &&
       C->isZero()) {
-    outs() << BB_LEFT->getName() << ", " << BB_RIGHT->getName() << "\n";
     //br i1 false, label %BB_LEFT, label %BB_RIGHT
+    outs() << "Case2" << BB_RIGHT->getName() << "\n";
     return {BB, BB_RIGHT};
-  } else if (match(I, m_Br(m_Value(COND), m_Value(LEFT), m_Deferred(LEFT)))) {
-    outs() << BB_LEFT->getName() << ", " << BB_RIGHT->getName() << "\n";
+  } else if (match(I, m_Br(m_Value(COND), m_BasicBlock(BB_LEFT), m_Deferred(BB_LEFT)))) {
     //br i1 COND, label %BB_LEFT, label %BB_LEFT
+    outs() << "Case3" << BB_LEFT->getName() << "\n";
+    return {BB, BB_LEFT};
+  } else if (match(I, m_UnconditionalBr(BB_LEFT))) {
+    //br label %BB_LEFT
+    outs() << "Case4" << BB_LEFT->getName() << "\n";
     return {BB, BB_LEFT};
   } else {
-    outs() << BB_LEFT->getName() << ", " << BB_RIGHT->getName() << "\n";
+    outs() << "Case5\n";
     return {nullptr, nullptr};
   }
 }
 
-PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+PreservedAnalyses MergeBasicBlocks::run(Function &F, FunctionAnalysisManager &FAM) {
   DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
 
   vector<pair<BasicBlock*, BasicBlock*>> BBPairToMerge;
   vector<BasicBlock> BBsToRemove; // Which basic blocks will be away?
 
   for (auto &BB : F) {
-    pair<BasicBlock*, BasicBlock*> mergePair;
+    pair<BasicBlock*, BasicBlock*> mergePair = classifyMergeType(&BB);
     if (mergePair.first != nullptr && mergePair.second != nullptr) {
       BBPairToMerge.push_back(mergePair);
     }
   }
-
   return PreservedAnalyses::all();
+}
+
+extern "C" ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "mergeBasicBlocks", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+          ArrayRef<PassBuilder::PipelineElement>) {
+          if (Name == "mergeBasicBlocks") {
+            FPM.addPass(MergeBasicBlocks());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
 }
