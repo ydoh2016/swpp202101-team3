055277ac3b77db17f9044a786fe089d9e5e339fe
Chavo-Kim <68835703+Chavo-Kim@users.noreply.github.com>
2021-05-09 19:42:48 +0900

    feat: modify ./run-filechecks for multiple folder && change make test

--- run-filechecks.sh
+++ run-filechecks.sh
@@ -8,9 +8,29 @@ fi
 echo "--- Start FileCheck.. ---"
 set -e
 
-for p in adce dae licm simplifyCFG tailCallElim; do
-for i in `find ./filechecks/${p} -name "*.ll"` ; do
-  echo $i
-  bin/sf-compiler $i .tmp.s ${p}
-  $1 $i < .tmp.s
+failed_opts=()
+
+for p in `ls -1 ./filechecks`; do
+  echo "== Testing Optimizer ${p} =="
+  for i in `find ./filechecks/${p} -name "*.ll"` ; do
+    echo $i
+    bin/sf-compiler $i .tmp.s ${p}
+    $1 $i < .tmp.s
+    if [ "$?" -eq 0 ]; then
+      echo 'PASSED'
+    else
+      failed_opts+=("$i")
+    fi
+  done
 done
+
+
+
+if [ "${#failed_opts[@]}" -ne 0 ]; then
+  echo "== Failed Test List =="
+  for i in "${failed_opts[@]}"; do
+    echo $i
+  done
+else
+  echo "== All Tests are PASSED! =="
+fi
