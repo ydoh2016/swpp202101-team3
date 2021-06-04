80d667eab1ae214e3772581c0634ba03d121698e
ObjectOrientedLife <smsychjy96@gmail.com>
2021-06-05 00:28:57 +0900

    Phi node addition

--- src/backend/Inlining.cpp
+++ src/backend/Inlining.cpp
@@ -7,6 +7,7 @@
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
 
 #include <vector>
 #include <algorithm>
@@ -21,47 +22,74 @@ void InliningPass::getFunctionCalls(Module *M, vector<CallInst*> *calls) {
             Instruction *I = &*IT;
             CallInst *CI = dyn_cast<CallInst>(I);
             if (CI != nullptr && CI->getCalledFunction()->hasExactDefinition()) {
-                outs() << CI->getName() << "\n";
                 calls->push_back(CI);
             }
         }
     }
 }
 
-void InliningPass::cloneIntoCaller(CallInst *call) {
+void InliningPass::cloneIntoCaller(CallInst *call, DominatorTree &DT) {
     Function *caller = call->getFunction();
     Function *callee = call->getCalledFunction();
-    divideBasicBlock(call);
-}
 
-void InliningPass::divideBasicBlock(Instruction *criteria) {
-    Function *caller = criteria->getFunction();
-    BasicBlock *BBCaller = criteria->getParent();
+    BasicBlock *BBCaller = call->getParent();
+    BasicBlock *BBAfter = SplitBlock(BBCaller, call, &DT);
 
-    ValueToValueMapTy VMAfter;
-    BasicBlock *BBAfter = CloneBasicBlock(BBCaller, VMAfter, "", caller);
+    BasicBlock *BBEntry; // The inlined entry block
+    PHINode *phiInserted = nullptr; // To prevent multiple phi node insertion
+    // Copy the inilined function's BBs into the caller function
+    for (auto &BB : *callee) {
+        ValueToValueMapTy VM;
+        BasicBlock *BBCopied = CloneBasicBlock(&BB, VM, "", caller);
 
-    vector<Instruction*> IAfter;
-    bool callInstSeen = false;
-    for (Instruction &I : *BBAfter) {
-        if (!callInstSeen) {
-            IAfter.push_back(&I);
+        if (&callee->getEntryBlock() == &BB) {
+            BBEntry = BBCopied;
         }
-        if (VMAfter[criteria] == &I) {
-            callInstSeen = true;
+
+        int argCount = call->getNumArgOperands();
+        for (int i = 0; i < argCount; ++i) {
+            Value *VArg = call->getArgOperand(i); // i'th argument to the inlined function
+            Value *VParam = callee->getArg(i); // i'th parameter of the inlined function
+            // Replace all uses of parameters in the copied block into the VArg
+            for (auto it = VParam->use_begin(), end = VParam->use_end(); it != end;) {
+                Use &U = *it++;
+                User *Usr = U.getUser();
+                Instruction *UsrI = dyn_cast<Instruction>(Usr);
+                if (UsrI->getParent() == BBCopied) {
+                    U.set(VArg);
+                }
+            }
+        }
+        
+        // If this block is a return block
+        Instruction *terminator = BBCopied->getTerminator();
+        if (dyn_cast<ReturnInst>(terminator) != nullptr) {
+            if (terminator->getNumOperands() != 0) {
+                Value *VReturn = terminator->getOperand(0);
+                if (phiInserted == nullptr) {
+                    phiInserted = PHINode::Create(call->getFunctionType(), 1, "");
+                    // Insert before the first non-phi instruction
+                    Instruction *insertPoint = BBAfter->getFirstNonPHI();
+                    phiInserted->insertBefore(insertPoint);
+                }
+                phiInserted->addIncoming(VReturn, BBCopied);
+            }
+            BranchInst::Create(BBAfter, BBCopied);
+            terminator->eraseFromParent();
         }
     }
 
-    for (Instruction *I : IAfter) {
-        I->eraseFromParent();
-    }
+    BBCaller->getTerminator()->setSuccessor(0, BBEntry);
+    call->eraseFromParent();
 }
 
 PreservedAnalyses InliningPass::run(Module &M, ModuleAnalysisManager &MAM) {
     vector<CallInst*> functionCalls;
     getFunctionCalls(&M, &functionCalls);
     for (auto call : functionCalls) {
-        cloneIntoCaller(call);
+        FunctionAnalysisManager &FAM = MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
+        DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(*call->getFunction());
+        cloneIntoCaller(call, DT);
     }
 
     return PreservedAnalyses::all();
