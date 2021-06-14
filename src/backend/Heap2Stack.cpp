eec8e931936a4c24b470d8aaa744cb464a0f3570
Chavo-Kim <68835703+Chavo-Kim@users.noreply.github.com>
2021-06-14 19:35:01 +0900

    fix: change order of passes

--- src/backend/Heap2Stack.cpp
+++ src/backend/Heap2Stack.cpp
@@ -73,8 +73,10 @@ PreservedAnalyses Heap2Stack::run(Function &F, FunctionAnalysisManager &FAM) {
         if(Function *callee = call_inst -> getCalledFunction()) {
           if(callee -> getName().str() == "malloc") {
             // found malloc
-            if(F.getNumUses()<=0)
+            if(F.getNumUses()<=0){
+              // outs() << "check 0: " << F.getName().str() << "\n";
               heap_allocation.push_back(call_inst);
+            }
             else {
               vector<Instruction*> chk;
               chk.push_back(&I);
